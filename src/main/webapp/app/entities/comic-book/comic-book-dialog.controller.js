7fd3f7e7ebc9617829d43ff94041fc6518fb98c9
Nifiro <nifiro@outlook.com>
2018-12-25 14:58:00 +0300

    Fix "New comic book" page

--- src/main/webapp/app/entities/comic-book/comic-book-dialog.controller.js
+++ src/main/webapp/app/entities/comic-book/comic-book-dialog.controller.js
@@ -1,13 +1,13 @@
-(function() {
+(function () {
     'use strict';
 
     angular
         .module('comicbooksApp')
         .controller('ComicBookDialogController', ComicBookDialogController);
 
-    ComicBookDialogController.$inject = ['$timeout', '$scope', '$stateParams', 'DataUtils', 'entity', 'ComicBook', 'Author'];
+    ComicBookDialogController.$inject = ['$timeout', '$scope', '$stateParams', '$mdDialog', 'DataUtils', 'entity', 'ComicBook', 'Author'];
 
-    function ComicBookDialogController ($timeout, $scope, $stateParams, DataUtils, entity, ComicBook, Author) {
+    function ComicBookDialogController($timeout, $scope, $stateParams, $mdDialog, DataUtils, entity, ComicBook, Author) {
         var vm = this;
 
         vm.comicBook = entity;
@@ -17,13 +17,15 @@
         vm.openFile = DataUtils.openFile;
         vm.save = save;
         vm.authors = Author.query();
+        vm.selectedAuthor = null;
 
-        $timeout(function (){
+        $timeout(function () {
             angular.element('.form-group:eq(1)>input').focus();
         });
 
-        function save () {
+        function save() {
             vm.isSaving = true;
+            vm.comicBook.authorId = vm.selectedAuthor.id;
             if (vm.comicBook.id !== null) {
                 ComicBook.update(vm.comicBook, onSaveSuccess, onSaveError);
             } else {
@@ -31,12 +33,31 @@
             }
         }
 
-        function onSaveSuccess (result) {
+        function onSaveSuccess(result) {
             $scope.$emit('comicbooksApp:comicBookUpdate', result);
             vm.isSaving = false;
+            var dialog = $mdDialog.alert()
+                .clickOutsideToClose(true)
+                .title('Ура!')
+                .textContent('Комикс "' + vm.comicBook.title + '" успешно добавлен')
+                .ok('OK');
+            $mdDialog.show(dialog);
+            vm.comicBook = {
+                title: null,
+                chapters: null,
+                description: null,
+                publisher: null,
+                serializedFrom: null,
+                serializedTo: null,
+                imagePath: null,
+                coverPath: null,
+                status: null,
+                id: null,
+                authorId: null
+            };
         }
 
-        function onSaveError () {
+        function onSaveError() {
             vm.isSaving = false;
         }
 
@@ -45,7 +66,7 @@
         vm.datePickerOpenStatus.createdDate = false;
         vm.datePickerOpenStatus.lastModifiedDate = false;
 
-        function openCalendar (date) {
+        function openCalendar(date) {
             vm.datePickerOpenStatus[date] = true;
         }
     }
